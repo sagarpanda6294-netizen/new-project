422eaf7b4cb477706e2f5265140fc21098b8d5b5
sagarpanda6294-netizen <sagarpanda6294@gmail.com>
2026-04-16 10:54:47 +0530

    newline

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+           int num1 = 10;   // first number
+        int num2 = 20;   // second number
+
+        int sum = num1 + num2;  // add them
+
+        System.out.println("The sum is: " + sum);
+              
+        System.out.println("The sum is: " + sum);
+
+    }
+}
+
+
